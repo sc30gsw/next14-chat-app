7a0ea3507fb0a60e50472f75740ea10a55746074
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-17 07:58:45 +0900

    💄 スタイリングの修正

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = ({ color = 'border-blue-500' }: { color?: string }) => {
   return (
-    <div className="my-16 flex justify-center">
+    <div className="h-full py-16 flex justify-center bg-slate-800">
       <div
         className={`h-10 w-10 animate-spin rounded-full border-4 ${color} border-t-transparent`}
       ></div>
