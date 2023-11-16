7216f6c3f716649e64248ae47193c405ce3a9671
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-16 12:37:55 +0900

    💄 チャットページの作成に伴うUIの修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 type ButtonProps = {
   type?: 'reset' | 'button' | 'submit'
   label: string
+  isChat?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ type, label }) => {
+const Button: React.FC<ButtonProps> = ({ type, label, isChat }) => {
   return (
     <button
       type={type}
-      className="text-white mt-5 bg-sky-400 h-12 py-0 px-8 rounded-md border-0 text-center shadow-md hover:opacity-70"
+      className={`text-white bg-sky-400 border-0 rounded-md text-center shadow-md hover:opacity-70 ${
+        isChat
+          ? 'text-sm sm:text-base h-[50px] ml-[15px] px-4 sm:w-[75px] w-3/12'
+          : 'mt-5 h-12 px-8'
+      }`}
     >
       {label}
     </button>
