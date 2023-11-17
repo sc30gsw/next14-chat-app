10749bb66716c76e86dc57329d43431bc226cf35
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-17 12:29:30 +0900

    ✨ メッセージ投稿機能の作成

--- src/components/messages/Form.tsx
+++ src/components/messages/Form.tsx
@@ -1,44 +1,64 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import type { SubmitHandler } from 'react-hook-form'
 import { useForm } from 'react-hook-form'
 
 import Button from '@/components/Button'
 import Input from '@/components/Input'
 import ImageUploader from '@/components/messages/ImageUploader'
+import useCreateMessage from '@/hooks/useCreateMessage'
 import type { MessageInput } from '@/types/MessageInput'
 import { messageScheme } from '@/types/MessageInput'
 
-const Form = () => {
+type FormProps = {
+  roomId: string
+}
+
+const Form: React.FC<FormProps> = ({ roomId }) => {
+  const [base64, setBase64] = useState('')
+
   const {
     handleSubmit,
     control,
-    watch,
-    setError,
     reset,
     formState: { isSubmitting },
   } = useForm<MessageInput>({
     resolver: zodResolver(messageScheme),
-    defaultValues: { message: '', image: '' },
+    defaultValues: { content: '', image: '' },
   })
 
-  const messageValue = watch('message')
+  const { handleCreateMessage } = useCreateMessage(roomId)
+
+  const onSubmit: SubmitHandler<MessageInput> = useCallback(
+    async (data) => await handleCreateMessage(data, reset, setBase64),
+    [handleCreateMessage, reset],
+  )
 
   return (
-    <form className="bg-gray-300 h-[90px] py-5 px-10 flex">
+    <form
+      className="bg-gray-300 h-[90px] py-5 px-10 flex"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="bg-white flex w-full relative">
         <Input
-          id="message"
-          name="message"
+          id="content"
+          name="content"
           type="text"
           label="type a message"
           control={control}
           disabled={isSubmitting}
-          required
           isChat
+          isRoom
         />
-        <ImageUploader existsMessage={messageValue ? true : false} />
+        <ImageUploader
+          base64={base64}
+          setBase64={setBase64}
+          name="image"
+          disabled={isSubmitting}
+          control={control}
+        />
       </div>
       <Button type="submit" label="送信" isChat />
     </form>
--- src/components/messages/ImageUploader.tsx
+++ src/components/messages/ImageUploader.tsx
@@ -1,30 +1,95 @@
 'use client'
 
-import React from 'react'
+import Image from 'next/image'
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import {
+  Controller,
+  type ControllerProps,
+  type FieldValues,
+  useController,
+} from 'react-hook-form'
 
 type ImageUploaderProps = {
-  existsMessage: boolean
+  base64: string
+  setBase64: (prev: string) => void
+  name: string
+  disabled?: boolean
 }
 
-const ImageUploader: React.FC<ImageUploaderProps> = ({ existsMessage }) => {
+type ExtendsImageUploaderProps<T extends FieldValues> = Omit<
+  ControllerProps<T>,
+  'render'
+> &
+  ImageUploaderProps
+
+const ImageUploader = <T extends FieldValues>({
+  base64,
+  setBase64,
+  name,
+  disabled,
+  control,
+  rules,
+}: ExtendsImageUploaderProps<T>) => {
+  const { field, fieldState } = useController<T>({ name, control, rules })
+  const { error } = fieldState
+
+  const handleDrop = useCallback(
+    (files: any) => {
+      const file = files[0]
+      const reader = new FileReader()
+      reader.onload = (e: any) => {
+        const base64String = e.target.result
+        setBase64(base64String)
+        field.onChange(base64String) // ここで base64 文字列をセット
+      }
+      reader.readAsDataURL(file)
+    },
+    [field, setBase64],
+  )
+
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 1,
+    onDrop: handleDrop,
+    disabled,
+    accept: {
+      'image/jpeg': [],
+      'image/png': [],
+    },
+  })
+
   return (
-    <label
-      htmlFor="fileInput"
-      className={`relative text-xs text-white p-3 w-[60px] my-[5px] mx-[10px] text-center ${
-        existsMessage
-          ? 'shadow-md bg-sky-500 transition duration-200 hover:opacity-70'
-          : 'bg-sky-200'
-      }`}
-    >
-      画像
-      <input
-        disabled={existsMessage}
-        value={undefined}
-        id="fileInput"
-        type="file"
-        className="absolute top-0 left-0 right-0 bottom-0 w-full h-full opacity-0"
-      />
-    </label>
+    <>
+      <div
+        {...getRootProps({
+          className:
+            'relative text-xs text-white p-3 w-[60px] my-[5px] mx-[10px] text-center cursor-pointer shadow-md bg-sky-500 transition duration-200 hover:opacity-70',
+        })}
+      >
+        {base64 && (
+          <div className="absolute bottom-11 left-0 right-0">
+            <Image src={base64} height="100" width="100" alt="Uploaded image" />
+          </div>
+        )}
+        画像
+        <Controller
+          name={name}
+          control={control}
+          rules={rules}
+          render={({ field }) => (
+            <>
+              <input
+                {...field}
+                {...getInputProps()}
+                value={undefined}
+                type="file"
+              />
+              {error && <span className="text-red-500">{error.message}</span>}
+            </>
+          )}
+        />
+      </div>
+    </>
   )
 }
 
