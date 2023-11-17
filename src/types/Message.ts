04bebf59193b4a0d2afc1dd2eea0045c9defbcbd
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-18 08:48:45 +0900

    🏷️ 型をreact-hook-formのものに合わせる

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -4,8 +4,8 @@ export type Message = {
   id: string
   userId: string
   roomId: string
-  content?: string
-  image?: string
+  content?: string | null
+  image?: string | null
   createdAt: string
   updatedAt: string
   user: User
