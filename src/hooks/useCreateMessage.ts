870db27e05889eca7ede9dc5056024e33dd73e49
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-17 17:15:41 +0900

    不要な記述や誤字の修正

--- src/hooks/useCreateMessage.ts
+++ src/hooks/useCreateMessage.ts
@@ -38,7 +38,7 @@ const useCreateMessage = (roomId: string) => {
       try {
         await fetch('/api/messages', {
           method: 'POST',
-          headers: { 'Content-Type': 'application/jso' },
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
             userId: currentUser?.id,
             roomId,
