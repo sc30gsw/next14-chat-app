65ac2bd35e379d4069918ecb3cfcb8502af9de48
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-11-17 12:25:41 +0900

    ✨ チャットルーム詳細ページの作成・API作成

---
+++ src/app/api/rooms/[roomId]/route.ts
@@ -0,0 +1,33 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+import prisma from '@/libs/prismadb'
+
+type RoomParams = {
+  params: { roomId: string }
+}
+
+export const GET = async (req: NextRequest, { params }: RoomParams) => {
+  try {
+    if (req.method !== 'GET')
+      return NextResponse.json({ message: 'Bad Request' }, { status: 405 })
+
+    if (!params.roomId)
+      return NextResponse.json({ message: 'Invalid ID' }, { status: 400 })
+
+    const room = await prisma.room.findUnique({
+      where: { id: params.roomId },
+      include: { roomUsers: true },
+    })
+
+    if (!room)
+      return NextResponse.json({ message: 'Room not found' }, { status: 404 })
+
+    return NextResponse.json(room, { status: 200 })
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 },
+    )
+  }
+}
